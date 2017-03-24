4d335919f9fc4958d48e8346857633fabdd1e30c
Jake Zeal <jake@jakezeal.com>
2017-03-24 13:36:13 -0700

    update factorial_iterative

--- source/recursion.py
+++ source/recursion.py
@@ -17,10 +17,18 @@ def factorial_iterative(n):
     if n < 0 or not isinstance(n, int):
         raise ValueError('factorial is undefined for n = {}'.format(n))
 
+    if n == 0:
+        return 1
+
     number = 1
-    for i in range(1, n+1):
+    for i in range(n, 1, -1):
         number = number * i
     return number
+
+    # number = 1
+    # for i in range(1, n+1):
+    #     number = number * i
+    # return number
 
 
 def factorial_recursive(n):
